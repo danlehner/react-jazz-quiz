eb8292628da390493c28cb6c84f6a2663591f31a
Dan Lehner <lehnerdr@gmail.com>
2021-07-27 09:19:26 -0400

    Updates

---
+++ precache-manifest.dcddfb8b275a4035de35e86241873b5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecbceb0438c5289363eef61b8e09de4",
+    "url": "/react-jazz-quiz/index.html"
+  },
+  {
+    "revision": "9f8fa80f2fff6dde094e",
+    "url": "/react-jazz-quiz/static/css/main.3fa08c82.chunk.css"
+  },
+  {
+    "revision": "a21d22be6de049607cae",
+    "url": "/react-jazz-quiz/static/js/2.cea15591.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-jazz-quiz/static/js/2.cea15591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8fa80f2fff6dde094e",
+    "url": "/react-jazz-quiz/static/js/main.88d899c3.chunk.js"
+  },
+  {
+    "revision": "747c913ba3e871e86374",
+    "url": "/react-jazz-quiz/static/js/runtime-main.ede95b17.js"
+  },
+  {
+    "revision": "7520a06ad78dbfb30cefdda76f27f235",
+    "url": "/react-jazz-quiz/static/media/clarendon.7520a06a.otf"
+  }
+]);
